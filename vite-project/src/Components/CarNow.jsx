607e3bedc4362172e5f799dc257c7116930bb560
nicolopez93 <nicolopezarq@gmail.com>
2024-02-16 20:28:37 -0300

    carNow

--- vite-project/src/Components/CarNow.jsx
+++ vite-project/src/Components/CarNow.jsx
@@ -4,12 +4,12 @@ import imgcar from '../assets/car.jpeg';
 
 export const CarNow = () => {
   return (
-    <div>
+    <div  className="carNow-container">
         <div> 
         <h1>Explora la libertad de poder Rentar un auto</h1>
         <h3>Maneja con cuidado</h3>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis assumenda tempore architecto, repellat similique porro laborum exercitationem minima consequuntur atque adipisci deserunt voluptate voluptatibus voluptatum labore optio autem harum enim.</p>
-        <button>RENTA TU AUTO</button>
+        <button  className="carNow-button">RENTA TU AUTO</button>
         </div>
         <img src={imgcar} alt='car'className="car-img" />
     </div>
