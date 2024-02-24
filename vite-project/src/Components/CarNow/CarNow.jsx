76d4f527bf8732829717af9ec16209fed52d0f87
Nicolopez93 <nicolopezarq@gmail.com>
2024-02-23 23:03:01 -0300

    final del dia 23

--- vite-project/src/Components/CarNow/CarNow.jsx
+++ vite-project/src/Components/CarNow/CarNow.jsx
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import imgAuto from '../../assets/auto.png';
-import { Renta } from '../Renta/Renta.jsx';
+
 import './carNow.css'
 
 export const CarNow = () => {
-  const [showRenta, setShowRenta] = useState(false);
-
-  const handleRentaButtonClick = () => {
-    setShowRenta(true);
-  };
-
+  
   return (
     <div className="carNow-container">
-      <div className='carNow-text'> 
-        <h1>Explora la libertad de poder rentar el auto que siempre soñaste!</h1>
-        <h3>Maneja con cuidado</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis assumenda tempore architecto, repellat similique porro laborum exercitationem minima consequuntur atque adipisci deserunt voluptate voluptatibus voluptatum labore optio autem harum enim.</p>
-        <button className="carNow-button" onClick={handleRentaButtonClick}>BUSCA TU AUTO!</button>
-      </div>
+      <div className="search-bar">
+          <input type="text" placeholder="Buscar autos..." />
+          <button className="buscar-autos-button">Buscar</button>
+        </div>
       <div className="carNow-image-container">
         <img src={imgAuto} alt='auto' className="auto-img" />
-        {showRenta && <Renta />}
-      </div>
+        </div>
     </div>
   );
 };
